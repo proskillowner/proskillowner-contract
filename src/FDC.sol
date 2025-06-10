666c9cdff73a724a9f9f5ecb0ce68a71b319f48c
proskillowner <proskillowner@gmail.com>
2025-06-10 23:19:07 +0000

    Update

--- src/FDC.sol
+++ src/FDC.sol
@@ -6,12 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "forge-std/console.sol";
 
 import "@interfaces/IUniswapV2Router.sol";
-import "@interfaces/IUniswapV3PoolActions.sol";
-import "@interfaces/IPancakeV3FlashCallback.sol";
 
-contract Bot is OwnableUpgradeable, IPancakeV3FlashCallback {
-    IUniswapV3PoolActions private USDT_WBNB_POOL = IUniswapV3PoolActions(0x172fcD41E0913e95784454622d1c3724f546f849);
-
+contract Bot is OwnableUpgradeable {
     IUniswapV2Router private constant UNISWAP_ROUTER = IUniswapV2Router(0x10ED43C718714eb63d5aA57B78B54704E256024E);
 
     IUniswapV2Pair private constant USDT_FDC_PAIR = IUniswapV2Pair(0x45D675Ff71FC8A08d27F631E99Af2AceD1d48903);
@@ -33,87 +29,7 @@ contract Bot is OwnableUpgradeable, IPancakeV3FlashCallback {
         to.transfer(address(this).balance);
     }
 
-    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        address recipient
-    ) public {
-        IERC20(tokenIn).approve(address(UNISWAP_ROUTER), amountIn);
-        address[] memory path = new address[](2);
-        path[0] = tokenIn;
-        path[1] = tokenOut;
-        UNISWAP_ROUTER.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-            amountIn, 0, path, recipient, block.timestamp
-        );
-        IERC20(tokenIn).approve(address(UNISWAP_ROUTER), 0);
-    }
-
-    function pancakeV3FlashCallback(uint256 fee0, uint256, bytes calldata data) external {
-        // uint256 borrowAmount = abi.decode(data, (uint256));
-
-        // console.log("Before buy");
-        // console.log("Pool USDT balance =>", USDT.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Pool FDC balance =>", FDC.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log();
-
-        // swapExactTokensForTokensSupportingFeeOnTransferTokens(
-        //     address(USDT), address(FDC), USDT.balanceOf(address(this)), address(this)
-        // );
-
-        // console.log("After buy");
-        // console.log("Before burn");
-        // console.log("Pool USDT balance =>", USDT.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Pool FDC balance =>", FDC.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Bot USDT balance =>", USDT.balanceOf(address(this)) / 1 ether);
-        // console.log("Bot FDC balance =>", FDC.balanceOf(address(this)) / 1 ether);
-        // console.log();
-
-        // console.log("After burn");
-        // console.log("Before sell");
-        // console.log("Pool USDT balance =>", USDT.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Pool FDC balance =>", FDC.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Bot USDT balance =>", USDT.balanceOf(address(this)) / 1 ether);
-        // console.log("Bot FDC balance =>", FDC.balanceOf(address(this)) / 1 ether);
-        // console.log();
-
-        // swapExactTokensForTokensSupportingFeeOnTransferTokens(
-        //     address(FDC), address(USDT), FDC.balanceOf(address(this)), address(this)
-        // );
-
-        // console.log("After sell");
-        // console.log("Pool USDT balance =>", USDT.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Pool FDC balance =>", FDC.balanceOf(address(USDT_FDC_PAIR)) / 1 ether);
-        // console.log("Bot USDT balance =>", USDT.balanceOf(address(this)) / 1 ether);
-        // console.log("Bot FDC balance =>", FDC.balanceOf(address(this)) / 1 ether);
-        // console.log();
-
-        // require(USDT.balanceOf(address(this)) > borrowAmount + fee0, "No profit");
-
-        // USDT.transfer(address(USDT_WBNB_POOL), borrowAmount + fee0);
-    }
-
-    // function main(uint256 flashLoanAmount, address withdrawAddress) public onlyOwner {
-    //     flashLoanAmount = flashLoanAmount * 1 ether;
-
-    //     bytes memory params = abi.encode(flashLoanAmount);
-
-    //     USDT_WBNB_POOL.flash(address(this), flashLoanAmount, 0, params);
-
-    //     USDT.transfer(withdrawAddress, USDT.balanceOf(address(this)));
-
-    //     console.log("Profit =>", (USDT.balanceOf(withdrawAddress)) / 1 ether);
-    //     console.log();
-    // }
-
-    function approve() public onlyOwner {
-        USDT.approve(address(UNISWAP_ROUTER), type(uint256).max);
-        FDC.approve(address(UNISWAP_ROUTER), type(uint256).max);
-        USDT.approve(address(USDT_FDC_PAIR), type(uint256).max);
-        FDC.approve(address(USDT_FDC_PAIR), type(uint256).max);
-    }
-
-    function register(address withdrawAddress) public {
+    function main() public {
         console.log("tx.origin =>", tx.origin);
         console.log("msg.sender =>", msg.sender);
         console.log("address(this) =>", address(this));
--- test/FDC.t.sol
+++ test/FDC.t.sol
@@ -5,6 +5,8 @@ import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../src/FDC.sol";
 
+import "@interfaces/IUniswapV2Router.sol";
+
 interface IMint {
     function userLpValues(address) external view returns (uint256);
     function mintStartTimes(address) external view returns (uint256);
@@ -15,6 +17,7 @@ contract BotTest is Test {
     ERC20 public constant USDT = ERC20(0x55d398326f99059fF775485246999027B3197955);
     ERC20 public constant FDC = ERC20(0xf0788dB035F1AEBF5E653f0692e72C0d34F9860C);
     IMint public constant MINT = IMint(0x50E27D405419de984d9EEe80068844A4ddA88D61);
+    IUniswapV2Router public constant UNISWAP_ROUTER = IUniswapV2Router(0x10ED43C718714eb63d5aA57B78B54704E256024E);
 
     receive() external payable {}
 
@@ -38,15 +41,27 @@ contract BotTest is Test {
 
         vm.signAndAttachDelegation(address(bot), privateKey);
 
-        bytes memory code = address(userAddress).code;
-        require(code.length > 0, "no code");
+        require(address(userAddress).code.length > 0, "no code");
 
-        Bot(payable(address(userAddress))).register(address(bot));
+        Bot(payable(address(userAddress))).main();
 
         uint256 usreLpValues = MINT.userLpValues(userAddress);
-        uint256 mintStartTime = MINT.mintStartTimes(userAddress);
-
         console.log("User LP Values =>", usreLpValues / 1 ether);
+
+        vm.warp(block.timestamp + 1 minutes);
+
+        vm.signAndAttachDelegation(address(bot), privateKey);
+
+        require(address(userAddress).code.length > 0, "no code");
+
+        address[] memory path = new address[](2);
+        path[0] = address(USDT);
+        path[1] = address(FDC);
+        UNISWAP_ROUTER.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            1 ether, 0, path, userAddress, block.timestamp
+        );
+
+        uint256 mintStartTime = MINT.mintStartTimes(userAddress);
         console.log("Mint Start Time =>", mintStartTime);
     }
 }
